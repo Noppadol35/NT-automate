be622c64141c24f0917554901ef06d0288bef506
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2025-04-10 08:45:12 +0700

    commit : backup config command

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from netmiko import ConnectHandler
 
 device = {
@@ -8,4 +9,36 @@ device = {
 }
 
 net_connect = ConnectHandler(**device)
-output = net_connect.send_command('sh run')
+
+
+net_connect.enable()
+
+# config commands
+
+config_commands = [
+    'interface ethernet 0/3',
+    'ip address 192.168.1.1 255.255.255.192',
+    'no shutdown'
+]
+
+net_connect.save_config()
+net_connect.send_config_set(config_commands)
+
+# check if the device is reachable
+try:
+    net_connect.find_prompt()
+except Exception as e:
+    print(f"Error: {e}")
+    exit(1)
+
+
+# save config to a file
+command = ['show running-config', 'show ip int br', 'show version']
+name_command = ['running-config', 'ip-int-br', 'version']
+for i,cmd in enumerate(command):
+    output = net_connect.send_command(cmd)
+    filename = f"backup_{name_command[i]}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+    with open(filename, "w") as file:
+        file.write(output)
+
+    print(f"Saved config to {filename}")
